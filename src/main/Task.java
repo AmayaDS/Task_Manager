141a5c88a80d9d6cca2b65f6368e179d169cec56
DESKTOP-SIIUHK0\hp <goathamiedesilva@gmail.com>
2023-09-11 19:51:30 +0530

    Intial Commit

---
+++ src/main/Task.java
@@ -0,0 +1,55 @@
+package main;
+public class Task {
+    private int taskId;
+    private String title;
+    private String description;
+    private String priority;
+    private String dueDate;
+    
+    public Task(int taskId,String title, String description, String priority, String dueDate){
+        this.taskId = taskId;
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.dueDate = dueDate;
+    }
+    
+
+	public int getTaskId(){
+        return taskId;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public String getPriority(){
+        return priority;
+    }
+    public String getDueDate(){
+        return dueDate;
+    }
+    public void setTaskId(int taskId){
+        this.taskId=taskId;
+    }
+    public void setTitle(String title){
+        this.title=title;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public void setPriority(String priority){
+        this.priority = priority;
+    }
+    public void setDueDate(String dueDate){
+        this.dueDate = dueDate;
+    }
+    
+    @Override
+    public String toString() {
+        return "TaskID: " + taskId + "\nTitle: " + title + "\nDescription: " + description + "\nPriority: " + priority + "\nDue Date: " + dueDate;
+    }
+    
+    
+}
---
+++ src/main/Task_Manager.java
@@ -0,0 +1,96 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Task_Manager {
+
+    private static ArrayList<Task> tasks = new ArrayList<>();
+    private static int tasksIdCounter = 1;
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Welcome to Task Manager");
+            System.out.println("1. Add tasks");
+            System.out.println("2. View tasks");
+            System.out.println("3. Delete Task");
+            System.out.println("4. Exit");
+            System.out.println("What do you want to do?");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    addTask(scanner);
+                    break;
+
+                case 2:
+                    viewTasks();
+                    break;
+
+                case 3:
+                    deleteTask(scanner);
+                    break;
+
+                case 4:
+                    System.out.println("Exiting Task Manager. Goodbye!");
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid Entry");
+            }
+        }
+    }
+
+    // Methods
+    private static void addTask(Scanner scanner) {
+        Task task = new Task(tasksIdCounter, null, null, null, null);
+        task.setTaskId(tasksIdCounter);
+        task.setTitle(getUserInput("Enter Task Title:", scanner));
+        task.setDescription(getUserInput("Enter Description:", scanner));
+        task.setPriority(getUserInput("Enter Task Priority:", scanner));
+        task.setDueDate(getUserInput("Enter Task DueDate:", scanner));
+
+        // Adding the task into the list
+        tasks.add(task);
+        System.out.println("Task added successfully.");
+        tasksIdCounter++;
+    }
+
+    private static String getUserInput(String prompt, Scanner scanner) {
+        System.out.print(prompt);
+        return scanner.next();
+    }
+
+    private static void viewTasks() {
+        System.out.println("Task List:");
+        for (Task task : tasks) {
+            System.out.println("Task ID: " + task.getTaskId());
+            System.out.println(task);
+        }
+    }
+
+    // Inside the TaskManager class
+    private static void deleteTask(Scanner scanner) {
+        System.out.print("Enter the task ID to delete: ");
+        int taskIdToDelete = scanner.nextInt();
+
+        // Find and remove the task with the specified ID
+        boolean taskFound = false;
+        for (Task task : tasks) {
+            if (task.getTaskId() == taskIdToDelete) {
+                tasks.remove(task);
+                taskFound = true;
+                System.out.println("Task deleted successfully.");
+                break; // Exit the loop after deleting the task
+            }
+        }
+
+        if (!taskFound) {
+            System.out.println("Task not found with the specified ID.");
+        }
+    }
+}
